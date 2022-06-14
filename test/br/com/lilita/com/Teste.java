8efeae5d7609d45c8fa209cd418250d02d8afa0a
crisholiveira <cris.ho.soares@gmail.com>
2022-06-14 20:00:40 -0300

    configuração do projeto

---
+++ test/br/com/lilita/com/Teste.java
@@ -0,0 +1,17 @@
+package br.com.lilita.com;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+	@Test
+	void testarSeIgualADois() {
+
+		int a = 1 + 1;
+		assertEquals(2, a);
+
+	}
+
+}
